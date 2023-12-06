d6c0103e649343bfa71ead3a5a21b47700b48731
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-06 21:27:38 +0800

    add timer component and styling

--- src/timer.js
+++ src/timer.js
@@ -2,9 +2,11 @@
 const Timer=(props)=>{
 
 return (
-    <div className='timer'>
+    <div className={props.working? 'timer-green': 'timer-blue'}>
       <h1>{props.working ? 'Work Time' : 'Break Time'}</h1>
-      <p>{Math.floor(props.countDown / 60)}:{String(props.countDown % 60).padStart(2, '0')}</p>
+      <div className='timer-text'>
+        <p>{Math.floor(props.countDown / 60)}:{String(props.countDown % 60).padStart(2, '0')}</p>
+     </div>
     </div>
 )
 

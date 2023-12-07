0e1a08d995c74e68fc3f48205da2dcce2b05ea85
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-12-07 16:52:03 +0800

    made app more modulear
    most compoenents were being imported into buttons which didn't make sense
    
    so I moved them to App.js along wiht useEffect and the main states.
    
    This was all components are rendered together

--- src/buttons.js
+++ src/buttons.js
@@ -1,71 +1,16 @@
 
 
-import React, { useState, useEffect } from 'react';
-import Timer from './timer';
-import Tracker from './tracker';
-import Ding from './ding'
-import Goal from './goal'
 
-const Buttons= ()=>{
-  const workDuration = 10; // 25 minutes in seconds
-  const breakDuration =5; // 5 minutes in seconds
-  const [working, setworking] = useState(true);
-  const [countDownTime, setCountDownTime] = useState(workDuration);
-  const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    let interval = null;
-
-    if (isActive && countDownTime > 0) {
-      interval = setInterval(() => {
-        setCountDownTime(countDownTime - 1); 
-      }, 1000);
-    }
-    
-    if (countDownTime === 0) {
-      const switchSession = !working;
-      setworking(switchSession);
-      setCountDownTime(switchSession ? workDuration : breakDuration);
-      setIsActive(true); // Optionally pause the timer on session switch
-    }
-
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, [isActive, countDownTime, working]);
-
-  const handlePause = () => {
-    setIsActive(false);
-  };
-
-  const handlePlay = () => {
-  console.log(`timer is starting`)
-    setIsActive(true);
-  };
-
-  const handleRestart = () => {
-    setCountDownTime(working ? workDuration : breakDuration);
-    setIsActive(false);
-  };
-   
+const Buttons= (props)=>{
 
     return(
     <div className='start'>
       <div>
-      <Timer 
-        working={working}
-        countDown={countDownTime}
-        />
-      <button onClick={handlePause}>Pause</button>
-      <button onClick={isActive ? handleRestart : handlePlay}>{isActive? 'restart' : 'Play'}</button>
+      
+      <button onClick={props.onPause}>Pause</button>
+      <button onClick={props.active ? props.onRestart : props.onPlay}>{props.active? 'restart' : 'Play'}</button>
       </div>
-      <Goal/>
-      <Tracker
-        working = {working}
-        active= {isActive}
-
-      />
-      <Ding 
-        time = {countDownTime}
-        />
     </div>
     
     )
--- src/timer.js
+++ src/timer.js
@@ -5,7 +5,7 @@ return (
     <div className={props.working? 'timer-green': 'timer-blue'}>
       <h1>{props.working ? 'Work Time' : 'Break Time'}</h1>
       <div className='timer-text'>
-        <h3>{Math.floor(props.countDown / 60)}:{String(props.countDown % 60).padStart(2, '0')}</h3>
+        <h3>{Math.floor(props.time / 60)}:{String(props.time % 60).padStart(2, '0')}</h3>
      </div>
     </div>
 )
